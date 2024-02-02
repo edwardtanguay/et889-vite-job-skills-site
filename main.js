e61147b7602625becc63f0920bfac84640ca6ef0
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-02 13:48:29 +0100

    showing number of jobs

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+import jobs from './data/jobs.json';
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Jobs and Skills</h1>
-  <p>Welcome to this site.</p>
+  <p>There are ${jobs.length} jobs.</p>
 `;
