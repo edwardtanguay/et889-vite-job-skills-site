8099fb4b9ec936639693bf0dbcad2d7baf857d00
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-02 14:31:22 +0100

    created component

---
+++ src/components/Jobs.js
@@ -0,0 +1,20 @@
+import jobs from '../data/jobs.json';
+
+export const Jobs = () => {
+
+	let jobsHtml = '<ul>';
+	for (const job of jobs) {
+		jobsHtml += `
+<li class="mb-4">
+  <p class="font-semibold">${job.title}</p>
+  <p>${job.company}</p>
+</li>
+`;
+	}
+	jobsHtml += '</ul>';
+
+	return jobsHtml;
+	// return /*html*/ `
+	// ${jobsHtml}
+	// `;
+};
