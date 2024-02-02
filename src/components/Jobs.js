b206ddacfb2f09485fd07e712cb5f376949ae252
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-02 15:12:16 +0100

    sort in

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,14 +1,21 @@
 import jobs from '../data/jobs.json';
+import skills from '../data/skills.json';
+
+const getSkillNameList = (skillList) => {
+	const skills = skillList.split(',').map(m => m.trim()).sort((a,b) => a > b ? 1 : -1);
+	console.log(skills);
+}
 
 export const Jobs = () => {
 
 	let jobsHtml = '<ul>';
 	for (const job of jobs) {
+
 		jobsHtml += `
 <li class="mb-4 bg-slate-600 p-3 rounded">
   <p class="font-semibold">${job.title}</p>
   <p class="text-orange-950">${job.company}</p>
-  <p>${job.skillList}</p>
+  <p>${getSkillNameList(job.skillList)}</p>
 </li>
 `;
 	}
