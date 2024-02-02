0264a096b2847ba153bf1b819de0d74c01bc3fce
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-02 15:40:45 +0100

    showing skills on jobs

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -2,9 +2,16 @@ import jobs from '../data/jobs.json';
 import skills from '../data/skills.json';
 
 const getSkillNameList = (skillList) => {
-	const skills = skillList.split(',').map(m => m.trim()).sort((a,b) => a > b ? 1 : -1);
-	console.log(skills);
-}
+	const skillIdCodes = skillList.split(',').map(m => m.trim()).sort();
+	const skillHtmlTexts = [];
+	for (const skillIdCode of skillIdCodes) {
+		const skill = skills.find(skill => skill.idCode === skillIdCode);
+		if (skill) {
+			skillHtmlTexts.push(`<a class="underline" target="_blank" href="${skill.url}">${skill.name}</a>`);
+		}
+	}
+	return skillHtmlTexts.join(', ');
+};
 
 export const Jobs = () => {
 
@@ -13,7 +20,7 @@ export const Jobs = () => {
 
 		jobsHtml += `
 <li class="mb-4 bg-slate-600 p-3 rounded">
-  <p class="font-semibold">${job.title}</p>
+  <p class="font-semibold"><a href="${job.url}" class="underline" target="_blank">${job.title}</a></p>
   <p class="text-orange-950">${job.company}</p>
   <p>${getSkillNameList(job.skillList)}</p>
 </li>
