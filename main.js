ec1458a4e9b7cc2060549e7f343639fca05f6e80
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-02 14:05:00 +0100

    showing jobs styled with css

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 import jobs from './data/jobs.json';
-import './style.css'
+import './style.css';
+
+let jobsHtml = '<ul>';
+for (const job of jobs) {
+  jobsHtml += `
+<li>
+  <p><b>${job.title}</b></p>
+  <p>${job.company}</p>
+</li>
+`;
+}
+jobsHtml += '</ul>';
 
 document.querySelector('#app').innerHTML = `
   <h1>Jobs and Skills</h1>
   <p>There are ${jobs.length} jobs.</p>
+  ${jobsHtml}
 `;
